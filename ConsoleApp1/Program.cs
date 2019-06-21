b061feb2e649a90f82f1c7271abac9dad710a008
Trieu Tran <troy.tran@hotmail.com>
2019-06-21 15:08:32 +0200

    Added more

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,8 @@ namespace ConsoleApp1
 
             Console.WriteLine("Hello World!---3");
             Console.WriteLine("Hello World!---3");
+            Console.WriteLine("Hello World!---3");
+
 
 
 
